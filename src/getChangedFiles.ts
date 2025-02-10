f5be06d409dcbaef981d64030bea41aef501d70c
sologeek <liyoushan3@gmail.com>
2025-02-10 09:05:33 +0800

    fix: 🐛 log changed files

--- src/getChangedFiles.ts
+++ src/getChangedFiles.ts
@@ -1,10 +1,12 @@
 import { getInput } from "@actions/core";
 
 export const getChangedFiles = async (): Promise<string[]> => {
-    const inputChangedFiles = getInput("changedFiles");
+    const inputChangedFiles = getInput("changed-files");
     if (!inputChangedFiles) {
         throw new Error("changedFiles is required");
     }
+
+    console.log("inputChangedFiles", inputChangedFiles);
 
     const changedFiles = inputChangedFiles.split("\n").map((file) => file.trim());
     console.log("changedFiles", changedFiles);
